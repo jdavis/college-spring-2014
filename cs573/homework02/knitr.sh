253fc915b7780d7ab1b65afb30c5d0931445389a
Josh Davis <josh@joshldavis.com>
2014-02-23 17:58:25 -0600

    Add knitr to bin/ remove from dir

--- cs573/homework02/knitr.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-PATTERN="*.Rnw"
-FILE="solution.Rnw"
-KNITR="echo \"Rerunning Knitr...\"; Rscript -e \"library(knitr); knit('./${FILE}')\""
-
-echo "Watching ${FILE}..."
-watchmedo shell-command --patterns="${PATTERN}" --command="${KNITR}" .
